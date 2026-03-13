6ba00a8b89b97625a41aa1fa646bcd02209457aa
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-03-13 17:18:30 +0700

    menambahkan main layout dan mendeklarasikan warna yang akan digunakan dan app bar

---
+++ lib/main layout.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MainLayout extends StatelessWidget {
+  static const Color primaryColor = Color(0xff4d51ed);
+  static const Color backgroundColor= Color(0xffffffff);
+  static const Color accentColor = Color(0xffffc107);
+  static const Color accentOrange = Color(0xffff7043);
+
+  static const Color textTitleColor= Color(0xff2d2d2d);
+  static const Color textSubtitleColor = Color(0xff666666);
+  static const Color inputFillColor = Color(0xfff8f9ffa);
+  static const Color InputBorderColor = Color(0xffe0e0e0);
+  static const Color labelColor = Color(0xff4a4a4a);
+
+  final Widget child;
+  final String title;
+  final bool showAppBar;
+  final List<Widget>? actions;
+
+  const MainLayout({
+    super.key,
+    required this.child,
+    this.title = '',
+   this.showAppBar= true,
+    this.actions,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
