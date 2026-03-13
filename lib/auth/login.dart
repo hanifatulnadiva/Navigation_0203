4a9b4dd44e52350bda350c65cfb5f3789d9033c4
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-03-13 20:29:32 +0700

    add text forgot password

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -133,6 +133,21 @@ class _LoginPageState extends State<LoginPage> {
                         return null;
                       },
                     ),
+                    Align(
+                      alignment:Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: (){},
+                      style:TextButton.styleFrom(
+                        foregroundColor: MainLayout.accentOrange),
+                      
+                      child:const Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600
+                        ),
+                      ) 
+                      ),
+                    )
                   ],
                 ),
               ),
